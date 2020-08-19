e3fe6c03dfb358ff02a3ea80c74bd5495d9f3654
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-08-19 10:26:55 -0400

    connect db

--- src/server.js
+++ src/server.js
@@ -1,10 +1,38 @@
 import express from 'express';
 import 'dotenv/config';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
 const { log, error } = console;
 
+mongoose.Promise = global.Promise
+
 const port = process.env.PORT || 3000;
+
+const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+
+const DB_URL = process.env.MONGO_URL;
+const TEST_DB_URL = process.env.MONGO_TEST_URL;
+
+if (process.env.NODE_ENV == "test") {
+    mongoose.connect(TEST_DB_URL, { useNewUrlParser: true }, (err) => {
+        if (err)
+            return log('Unable to Connect to MongoDB')
+        return log('Connection Successful to test DB')
+    })
+} else {
+    mongoose.connect(DB_URL, { useNewUrlParser: true }, (err) => {
+        if (err)
+            return log('Unable to Connect to MongoDB')
+        return log('Connection Successful')
+    })
+}
 
 app.listen(port, () => log("server is running"));
