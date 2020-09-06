e885adf807e545160c5d9b9da485194bc0a20592
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-09-06 11:39:26 -0400

    update validation

--- src/validator/meeting.js
+++ src/validator/meeting.js
@@ -6,7 +6,7 @@ const schema = Joi.object({
   date: Joi.number().required(),
   content: Joi.string().min(5).max(700).required(),
   isDeleted: Joi.boolean(),
-  attendees: Joi.string().min(1).max(100).required(),
+  attendees: Joi.string().min(1).max(25).required(),
   units: Joi.objectId(),
   supervisionType: Joi.objectId(),
   approval: Joi.objectId(),
